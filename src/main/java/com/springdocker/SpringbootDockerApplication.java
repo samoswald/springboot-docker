82f91845d36285f2a17abd6c07cf11d0235d388b
samoswald <aka.samdev@gmail.com>
2023-02-12 12:39:59 +0530

    api

--- src/main/java/com/springdocker/SpringbootDockerApplication.java
+++ src/main/java/com/springdocker/SpringbootDockerApplication.java
@@ -2,12 +2,20 @@ package com.springdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringbootDockerApplication {
 
+	@GetMapping("/msg")
+	public String hello(){
+		return "dockerize spring boot app";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDockerApplication.class, args);
 	}
+
 
 }
